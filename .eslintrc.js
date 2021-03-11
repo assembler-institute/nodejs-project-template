62c96bd517f1f874b09ef274e360cb55499a1a11
Dani Lucaci <danilucaci@gmail.com>
2021-03-11 09:50:22 +0100

    Initial commit

---
+++ .eslintrc.js
@@ -0,0 +1,31 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true,
+    node: true,
+    jest: true,
+    jquery: true,
+  },
+  extends: [
+    "eslint:recommended",
+    "plugin:compat/recommended",
+    "plugin:jest/recommended",
+    "prettier",
+    "prettier/prettier",
+  ],
+  parser: "@babel/eslint-parser",
+  parserOptions: {
+    ecmaVersion: 12,
+    sourceType: "module",
+    requireConfigFile: "false",
+  },
+  plugins: ["@html-eslint", "jest"],
+  overrides: [
+    {
+      files: ["*.html"],
+      parser: "@html-eslint/parser",
+      extends: ["plugin:@html-eslint/recommended"],
+    },
+  ],
+  rules: {},
+};
